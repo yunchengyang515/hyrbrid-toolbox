2c7982d0eba59d882cab183926279b4bd01b65e9
Yuncheng <fabioyang781@gmail.com>
2024-12-16 13:14:36 +1100

    fix some lint issue

--- components/Library/tabs/Workout.tsx
+++ components/Library/tabs/Workout.tsx
@@ -7,32 +7,6 @@ import { Workout } from '@/types/Workout'
 import WorkoutModal from '../modals/Workout/Workout'
 
 // Mock Workout Data
-const mockWorkouts = [
-  {
-    id: '1',
-    name: 'Full Body Strength',
-    description: 'A high-intensity strength training session.',
-    duration_minute: 60,
-    intensity: 8,
-    type: 'Strength',
-  },
-  {
-    id: '2',
-    name: 'Cardio Blast',
-    description: 'A quick and intense cardio workout.',
-    duration_minute: 30,
-    intensity: 7,
-    type: 'Cardio',
-  },
-  {
-    id: '3',
-    name: 'Core Stability',
-    description: 'Focus on core strength and stability.',
-    duration_minute: 45,
-    intensity: 6,
-    type: 'Core',
-  },
-]
 
 const workoutApiService = new WorkoutApiService()
 
